55ea27d69f41bc513f03621fe55f8d21e9b65745
MMORONTO <t2wycemo@gmail.com>
2021-07-08 16:57:17 -0400

    install material-ui. Layout app component elements

--- global-covid-tracker/src/App.js
+++ global-covid-tracker/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>
-        Buid a Global COVID-19 Dashboard
-      </h1>
+      <h1>GLOBAL COVID-19 DASHBOARD</h1>
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+      </FormControl>
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
