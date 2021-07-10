c17b9363535824542641b9f4aa5603a6132fc676
MMORONTO <t2wycemo@gmail.com>
2021-07-10 17:23:27 -0400

    set logic for looping through form dropdown menu options

---
+++ global-covid-tracker/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h1>GLOBAL COVID-19 DASHBOARD</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and 
+            show a drop down list of the options */}
+
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+
+      {/* Table */}
+      {/* Graphs */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
+
+// Add this in your component file
+require('react-dom');
+window.React2 = require('react');
+console.log(window.React1 === window.React2);
