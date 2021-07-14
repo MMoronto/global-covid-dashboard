b28b3f2a5ced9aea6b64fb296bfc2e6906b0eb09
MMORONTO <t2wycemo@gmail.com>
2021-07-14 17:03:43 -0400

    listen for clicked item from list pulled from API. log to console.

--- global-covid-tracker/src/App.js
+++ global-covid-tracker/src/App.js
@@ -6,25 +6,12 @@ function App() {
   const [countries, setCountries] = useState(["USA", "Italy", "Nigeria"]);
   const [country, setCountry] = useState('worldwide')
 
-  // STATE = How to write a variable in REACT <<<<<<<<
-
-  // https://disease.sh/v3/covid-19/countries
-
-  // USEEFFECT = Runs a piece of code based on a given condition
 
   useEffect(() => {
-    // The code inside here will run once 
-    // when the component loads and not again after
-
-    // async -> send a request, wait for it, do something with info
-
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
       .then((response) => AuthenticatorResponse.json())
-      .then((data) => {
-
-        
-
+      .then((data) => { 
         const countries = data.map((country) => (
           {
             name: country.country, // United STates, United Kingdom
@@ -38,12 +25,18 @@ function App() {
     getCountriesData();
   }, []); 
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    console.log("RANDOM Country >>> Test", countryCode);
+  }
+
   return (
     <div className="app">
       <div className="app__header">
         <h1>GLOBAL COVID-19 DASHBOARD</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country}>
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
             <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
