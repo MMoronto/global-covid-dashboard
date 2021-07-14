3a42e8fb45ceddf15ffd6f412b55635dd987ab40
MMORONTO <t2wycemo@gmail.com>
2021-07-14 17:53:36 -0400

    style component elements

--- global-covid-tracker/src/InfoBox.js
+++ global-covid-tracker/src/InfoBox.js
@@ -3,13 +3,18 @@ import { Card, CardContent, Typography } from "@material-ui/core";
 
 function InfoBox({ title, cases, total }) {
     return (
-        <Card>
+        <Card className="infoBox">
             <CardContent>
                 {/* Title i.e. Coronavirus cases */}
+                <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
 
                 {/* +120k Number of cases */}
+                <h2 className="infoBox__cases">{cases}</h2>
 
                 {/* 1.2M Total cases */}
+                <Typography className="infoBox__total" color="textSecondary">
+                    {total} Total
+                </Typography>
             </CardContent>
         </Card>
     )
