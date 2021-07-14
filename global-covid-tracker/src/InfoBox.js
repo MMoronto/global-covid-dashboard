24527a18409b84e589502c03437f9772f8f5ed0e
MMORONTO <t2wycemo@gmail.com>
2021-07-14 17:41:22 -0400

    import elements from material-ui

--- global-covid-tracker/src/InfoBox.js
+++ global-covid-tracker/src/InfoBox.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Card, CardContent, Typography } from "@material-ui/core";
 
 function InfoBox({ title, cases, total }) {
     return (
-        <div>
+        <Card>
             
-        </div>
+        </Card>
     )
 }
 
