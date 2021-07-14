6da47dea0bfb6d617312e29b3ca8563765c183bd
MMORONTO <t2wycemo@gmail.com>
2021-07-14 17:45:40 -0400

    flesh out card content and structure

--- global-covid-tracker/src/InfoBox.js
+++ global-covid-tracker/src/InfoBox.js
@@ -4,7 +4,13 @@ import { Card, CardContent, Typography } from "@material-ui/core";
 function InfoBox({ title, cases, total }) {
     return (
         <Card>
-            
+            <CardContent>
+                {/* Title i.e. Coronavirus cases */}
+
+                {/* +120k Number of cases */}
+
+                {/* 1.2M Total cases */}
+            </CardContent>
         </Card>
     )
 }
