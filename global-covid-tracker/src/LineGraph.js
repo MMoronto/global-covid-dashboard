6a8cedeba4d27c2a2dcc5c91b25804ae4d9dbf8d
MMORONTO <t2wycemo@gmail.com>
2021-07-19 16:10:18 -0400

    render chart based on api chartdata

--- global-covid-tracker/src/LineGraph.js
+++ global-covid-tracker/src/LineGraph.js
@@ -7,37 +7,51 @@ function LineGraph() {
     // https://disease.sh/v3/..
     //covid-19/historical/all?lastdays=120
 
-    useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-        .then(response => response.json())
-        .then(data => {
-    console.log(data);
-
-        });
-    }, []);
 
     const buildChartData = (data, casesType='cases') => {
         const chartData = [];
         let lastDataPoint;
-        data[casesType].cases.forEach(date => {
+        for(let date in data.cases) {
             if (lastDataPoint) {
                 const newDataPoint = {
                     x: date,
                     y: data[casesType][date] - lastDataPoint
-                }
+                };
                 chartData.push(newDataPoint);
             }
             lastDataPoint = data[casesType][date];
-        })
+        }
         return chartData;
-    }
+    };
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then((response) => response.json())
+        .then((data) => {
+            let chartData = buildChartData(data, 'cases');
+            console.log(chartData);
+
+            SVGMetadataElement(chartData);
+        });
+    }, []);
 
     return (
         <div>
             <h1>I'm a graph</h1>
+            <Line 
+                data={{
+                    datasets: [
+                        {
+                            backgroundColor: "rgba(204, 16, 52, 0.3",
+                            borderColor: "#CC1034",
+                            data: data,
+                        },
+                ],
+                }}
+            />
             {/* <Line data options /> */}
         </div>
-    )
+    );
 }
 
 export default LineGraph
