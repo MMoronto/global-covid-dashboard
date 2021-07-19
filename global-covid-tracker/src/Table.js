ef06f38e8c127e3c47d3f7653d97be39a3d13be3
MMORONTO <t2wycemo@gmail.com>
2021-07-19 11:16:52 -0400

    destructure country data pulled from api into country and cases

--- global-covid-tracker/src/Table.js
+++ global-covid-tracker/src/Table.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 function Table({ countries }) {
-    return <div className="table">
-        {countries.map(country => (
-            <tr>
-                <td></td>
-                <td></td>
-            </tr>
-
-        ))}
-    </div>;
+    return (
+        <div className="table">
+            {countries.map(({country, cases}) => (
+                <tr>
+                    <td>{country}</td>
+                    <td>
+                        <strong>{cases}</strong>
+                    </td>
+                </tr>
+            ))}
+        </div>
+    );
 }
 
 export default Table;
