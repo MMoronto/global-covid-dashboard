4cc62705b50e1d5e0e443fda2e7ad4e630e31e99
MMORONTO <t2wycemo@gmail.com>
2021-07-19 15:33:29 -0400

    call specified data range from api

--- global-covid-tracker/src/LineGraph.js
+++ global-covid-tracker/src/LineGraph.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Line } from "react-chartjs-2";
 
 function LineGraph() {
+    const [data, setDate] = useState({});
+
+    // https://disease.sh/v3/..
+    //covid-19/historical/all?lastdays=120
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+        .then(response => response.json())
+        .then(data => {
+            // 
+        })
+    }, [])
+
     return (
         <div>
-            
+            <Line data options />
         </div>
     )
 }
