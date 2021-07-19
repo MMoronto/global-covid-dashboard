3236d41365a72ce519c62a7493310e9ceefd7605
MMORONTO <t2wycemo@gmail.com>
2021-07-19 11:07:30 -0400

    go through list of countries and map returned country data to country & cases in table rows

--- global-covid-tracker/src/Table.js
+++ global-covid-tracker/src/Table.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-function Table() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Table({ countries }) {
+    return <div className="table">
+        {countries.map(country => (
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+
+        ))}
+    </div>;
 }
 
-export default Table
+export default Table;
