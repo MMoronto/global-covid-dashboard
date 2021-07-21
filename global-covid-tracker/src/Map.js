95b824b1787b323df30c5834a7b1ee26428900a2
MMORONTO <t2wycemo@gmail.com>
2021-07-20 22:33:21 -0400

    import LeafletMap from react-leaflet

--- global-covid-tracker/src/Map.js
+++ global-covid-tracker/src/Map.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 
 function Map() {
     return (
         <div className="map">
+
             <h1>I am a map</h1>
         </div>
-    )
+    );
 }
 
-export default Map
+export default Map;
 
