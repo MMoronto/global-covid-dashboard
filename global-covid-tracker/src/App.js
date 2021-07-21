acebcaf1b260aedb2c9adee6290c597765fbca31
MMORONTO <t2wycemo@gmail.com>
2021-07-20 22:58:11 -0400

    set map zoom and coordinate values and pass them into map component

--- global-covid-tracker/src/App.js
+++ global-covid-tracker/src/App.js
@@ -13,6 +13,8 @@ function App() {
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796});
+  const [mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -93,7 +95,10 @@ function App() {
             total={countryInfo.deaths} 
           />
         </div>
-        <Map />
+        <Map 
+          center={mapCenter}
+          zoom={mapZoom}
+        />
       </div>
       <Card className="app__right">
         <CardContent>
